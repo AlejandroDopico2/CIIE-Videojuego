b12b54bfc4056d86a68650a5ad8472d74942b10d
AbelJuncal <abel.juncal@udc.es>
2023-02-12 23:29:30 +0100

    Add pygame examples

---
+++ juegos_prueba/penguin/prueba.py
@@ -0,0 +1,51 @@
+import pygame
+from pygame.locals import *
+
+# ----------------------------------------------
+# Constantes, como anchos y largo de pantalla, etc.
+# ----------------------------------------------
+
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+# ----------------------------------------------
+# Clases y Funciones utilizadas (lo explicare en la siguiente parte)
+# ----------------------------------------------
+
+def main():
+    pygame.init()
+    
+    # creamos la ventana y le indicamos un titulo:
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("club penguin")
+
+    fondo = pygame.image.load("fondo.jpg").convert()
+    tux = pygame.image.load("tux.png").convert_alpha()
+
+    tux_pos_x = 550
+    tux_pos_y = 200
+
+    screen.blit(fondo, (0, 0))
+    screen.blit(tux, (tux_pos_x, tux_pos_y))
+
+    pygame.display.flip()
+
+    while True:
+        # Posibles entradas del teclado y mouse
+
+        tux_pos_x = tux_pos_x - 1
+
+        if tux_pos_x < 1:
+            tux_pos_x = 550
+
+        screen.blit(fondo, (0, 0))
+        screen.blit(tux, (tux_pos_x, tux_pos_y))
+
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+if __name__ == "__main__":
+    main()
