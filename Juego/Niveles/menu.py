6231d87cbea1b038ede92339a6bf00481fe82fef
AbelJuncal <abel.juncal@udc.es>
2023-02-27 12:23:07 +0100

    Added level selection menu

--- Juego/Niveles/menu.py
+++ Juego/Niveles/menu.py
@@ -70,6 +70,62 @@ class Pantalla():
             else:
                 value.text = value.font.render(value.text_input, True, value.base_color)
 
+class PantallaNiveles(Pantalla):
+    def __init__(self, pantalla):
+        Pantalla.__init__(self,pantalla)
+
+        MENU_TEXT = self.get_font(100).render("SELECT LEVEL", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+        self.screenTexts.append((MENU_TEXT, MENU_RECT))
+
+        LEVEL_ONE_BUTTON = Button(image=pygame.image.load("Recursos/Play Rect.png"), pos=(640, 220),
+                            text_input="LEVEL 1", font=self.get_font(75), base_color="#d7fcd4", hovering_color="White")
+        
+        self.screenButtons.update({"LEVEL_ONE":LEVEL_ONE_BUTTON})
+        
+        LEVEL_TWO_BUTTON = Button(image=pygame.image.load("Recursos/Play Rect.png"), pos=(640, 330),
+                    text_input="LEVEL 2", font=self.get_font(75), base_color="#d7fcd4", hovering_color="White")
+        
+        self.screenButtons.update({"LEVEL_TWO":LEVEL_TWO_BUTTON})
+        
+        LEVEL_THREE_BUTTON = Button(image=pygame.image.load("Recursos/Play Rect.png"), pos=(640, 440),
+                            text_input="LEVEL 3", font=self.get_font(75), base_color="#d7fcd4", hovering_color="White")
+        
+        self.screenButtons.update({"LEVEL_THREE":LEVEL_THREE_BUTTON})
+
+        BACK = Button(image=None, pos=(640, 630),
+                        text_input="BACK", font=self.get_font(65), base_color="#d7fcd4", hovering_color="White")
+
+        self.screenButtons.update({"BACK":BACK})
+
+    def get_font(self,size):
+        return pygame.font.Font("Recursos/font.ttf", size)
+
+    def eventsLoop(self, lista_eventos):
+        position = pygame.mouse.get_pos()
+        self.changeColor(position)
+        pygame.draw.circle(self.pantalla, (0, 255, 0),   position, 15, 1)
+        for event in lista_eventos:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if self.screenButtons["LEVEL_ONE"].checkForInput(position):
+                    self.menu.playLevel()
+                if self.screenButtons["LEVEL_TWO"].checkForInput(position):
+                    print('level chu')
+                if self.screenButtons["LEVEL_THREE"].checkForInput(position):
+                    print('level three')
+                if self.screenButtons["BACK"].checkForInput(position):
+                    self.menu.pantallaActual = 0   
+
+    def draw(self, pantalla):
+        self.pantalla.fill("black")
+
+        for text in self.screenTexts:
+            self.pantalla.blit(text[0], text[1])
+
+        for button in self.screenButtons:
+            self.pantalla.blit(self.screenButtons[button].text, self.screenButtons[button].text_rect)
+
 class PantallaInicio(Pantalla):
     def __init__(self, pantalla):
         Pantalla.__init__(self, pantalla)
@@ -104,7 +160,7 @@ class PantallaInicio(Pantalla):
         for event in lista_eventos:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if self.screenButtons["PLAY"].checkForInput(position):
-                    self.menu.playLevel()
+                    self.menu.pantallaActual = 2
                 if self.screenButtons["OPTIONS"].checkForInput(position):
                     self.menu.pantallaActual = 1
                 if self.screenButtons["QUIT"].checkForInput(position):
@@ -191,6 +247,7 @@ class Menu(PygameScene):
 
         self.listaPantallas.append(PantallaInicio(self))
         self.listaPantallas.append(PantallaOpciones(self))
+        self.listaPantallas.append(PantallaNiveles(self))
 
         self.mostrarPantallaInicial()
 
